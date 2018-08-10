1b12eab05bb7abac791dc3eea9bf0d61f12eb67f
Martin Kim <martinkim7677@gmail.com>
2018-08-10 14:48:10 -0400

    Controlled Inputs for Filtes

--- src/app.js
+++ src/app.js
@@ -11,15 +11,18 @@ import './styles/style.scss';
 
 const store = configureStore();
 
-store.subscribe(() => {
-  const state = store.getState();
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-  console.log(visibleExpenses);
-});
-
 store.dispatch(addExpense({ description: 'Water Bill', amount: 100, createdAt: -21000 }));
 store.dispatch(addExpense({ description: 'Gas Bill', amount: 200, createdAt: -1000 }));
-store.dispatch(setTextFilter('bill'));
+store.dispatch(setTextFilter('water'));
+
+setTimeout(() => {
+  store.dispatch(setTextFilter('bill'));
+}, 3000);
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
 
 const jsx = (
   <Provider store={store}>
