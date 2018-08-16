18c3491be99cc71d754e14abc5b9f0677ad1e1ed
Martin Kim <martinkim7677@gmail.com>
2018-08-16 12:58:38 -0400

    Filtering by Dates

--- src/app.js
+++ src/app.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter.js';
+import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses.js';
-import { setTextFilter } from './actions/filters.js';
-import getVisibleExpenses from './selectors/expenses.js';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
-import './styles/style.scss';
+import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: 100, createdAt: -21000 }));
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 200, createdAt: -1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 2000, createdAt: -5000 }));
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
-)
+);
 
 ReactDOM.render(jsx, document.getElementById('app'));
-
